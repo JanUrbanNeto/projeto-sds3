e3044b81d951749ecc139c48e89ff0bf1f7f9851
Jan Urban Neto <jan.urbanneto@gmail.com>
2021-09-10 18:46:03 -0300

    First request

--- frontend/src/components/donutChart/index.tsx
+++ frontend/src/components/donutChart/index.tsx
@@ -1,21 +1,43 @@
+import axios from "axios";
 import Chart from "react-apexcharts";
+import { SaleSum } from "types/Sale";
+import { BASE_URL } from "utils/request";
+
+type ChartData = {
+   labels: string[],
+   series: number[]
+}
 
 const DonutChart = () => {
+
+   let chartData : ChartData = { labels: [], series: [] };
+
+   axios.get(`${BASE_URL}/sales/sum-by-seller`)
+      .then((response) => {
+         const data = response.data as SaleSum[];
+         const myLabels = data.map(x => x.sellerName);
+         const mySeries = data.map(x => x.sum);
+
+         //console.log(response.data);
+      });
+
    const options = {
       legend: {
          show: true
       }
    };
 
-   const mockData = {
+/*   const mockData = {
+
       series: [477138, 499928, 444867, 220426, 473088],
       labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
    };
+*/
 
    return (
       <Chart
-         options={{ ...options, labels: mockData.labels }}
-         series={mockData.series}
+         options={{ ...options, labels: chartData.labels }}
+         series={chartData.series}
          type="donut"
          height="240"
       />
